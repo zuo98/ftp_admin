9815d35a41e8bef3788c57832c489a0e21e00a1d
zuo98 <1620258397@qq.com>
2020-08-16 22:46:57 +0800

    create ftp_admin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <el-row>
+      <el-button disabled>默认按钮</el-button>
+      <el-button type="primary" disabled>主要按钮</el-button>
+      <el-button type="success" disabled>成功按钮</el-button>
+      <el-button type="info" disabled>信息按钮</el-button>
+      <el-button type="warning" disabled>警告按钮</el-button>
+      <el-button type="danger" disabled>危险按钮</el-button>
+    </el-row>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
